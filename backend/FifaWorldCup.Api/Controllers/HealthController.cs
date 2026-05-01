ff3d90e03bbc97d811be8c4622f4f33f4d871ad0
Jerry <jerrytam221@gmail.com>
2026-05-01 21:07:27 +0700

    Add health check API

---
+++ backend/FifaWorldCup.Api/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FifaWorldCup.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "OK",
+                message = "FIFA World Cup API is running",
+                time = DateTime.Now
+            });
+        }
+    }
+}
